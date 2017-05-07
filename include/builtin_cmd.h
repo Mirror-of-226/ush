7e09707c011f91f82698b294ad929c72906fe04c
Chriskuei <chenjiangui@outlook.com>
2017-05-07 12:34:53 +0800

    Add command “ucd”

--- include/builtin_cmd.h
+++ include/builtin_cmd.h
@@ -2,7 +2,8 @@
 #define _BUILTIN_FUNC_H
 
 class builtin_cmd {
-    virtual void run(int argc, char *argv[]) = 0;
+public:
+    virtual void run(int argc, char *argv[]) {}
 };
 
 //builtin command types
@@ -10,26 +11,22 @@ class ucp: public builtin_cmd
 {
 public:
     void run(int argc, char *argv[]);
-
-
     void setTime(char *path, struct stat st);
-
-
     void copyFile(char *src, char *dst);
-   
-
     void walkDir(char *src, char *dst);
-    
-
     void copyDir(char *src, char *dst);
-   
-
     void copySymLink(char *src, char *dst);
-    
+};
+
+class ucd: public builtin_cmd
+{
+public:
+    void run(int argc, char *argv[]);
+
 };
 
 class test : public builtin_cmd {
-public:	
+public:
 	void run(int argc, char *argv[]);
 };
 
---
+++ ush.cpp
@@ -0,0 +1,41 @@
+#include "include\ush.h"
+#include "include\parser.h"
+
+using namespace std;
+
+void ush::loadMBF() {
+	MBC.insert(pair<string, builtin_cmd*>("test",new test()));
+}
+
+ush::ush()
+{
+	loadMBF();
+	while (gets_s(input_buf)) {
+		int num = 0;
+		char **s = Parser::run(input_buf, num);
+		for (int i = 0; i < num; i++) {
+			printf("%s\n",s[i]);
+		}
+		int a = 1;
+		char *a1[] = { "China","French","America","German" };
+		MBC[s[0]]->run(a,a1);
+	}
+}
+
+
+ush::~ush()
+{
+}
+
+int main() {
+	/*builtin_cmd* bt = new test();
+	int a = 1;
+	char *a1[] = { "China","French","America","German" };
+	bt->run(a, a1);*/
+	ush* u1 = new ush();
+	/*test* t1 = new test();
+	char *a[] = { "China","French","America","German" };
+	t1->run(1, a);
+	system("pause");*/
+	system("pause");
+}
